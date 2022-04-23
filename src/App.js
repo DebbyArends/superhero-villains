e5ac6a3fc2009022a97cd23e65b3ee0733354eea
DebbyArends <debby_arends@live.nl>
2022-04-23 21:55:19 +0200

    banner aanpassingen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { Switch, Route, Redirect} from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Navigation from "./Components/Navigation/Navigation";
 import HomePage from "./Pages/Homepage/Homepage";
 import Appearance from "./Pages/Appearance/Appearance";
@@ -42,23 +41,6 @@ function App() {
               </Route>
           </Switch>
       </div>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
