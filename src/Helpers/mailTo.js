59c0378363372e8d2788977935ced4b3c75a22d0
DebbyArends <debby_arends@live.nl>
2022-05-29 15:55:05 +0200

    eerste opzet powerstats

--- src/Helpers/mailTo.js
+++
@@ -1,7 +0,0 @@
-export const mailTo = ({ email, subject = '', body = '', children }) => {
-    let params = subject || body ? '?' : '';
-    if (subject) params += `subject=${encodeURIComponent(subject)}`;
-    if (body) params += `${subject ? '&' : ''}body=${encodeURIComponent(body)}`;
-
-    return <a href={`mailto:${email}${params}`}>{children}</a>;
-};
