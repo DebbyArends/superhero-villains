e99fb7bc3fad313adf62a213bb6ad0f6d269dce3
DebbyArends <debby_arends@live.nl>
2022-06-13 17:07:59 +0200

    character biography functie werkend gemaakt

--- src/Pages/CharacterBiography/CharacterBiography.js
+++ src/Pages/CharacterBiography/CharacterBiography.js
@@ -1,78 +1,66 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-// import CharacterBiographyCard from "../../Components/CharacterBiographyCard/CharacterBiographyCard";
+import CharacterBiographyCard from "../../Components/CharacterBiographyCard/CharacterBiographyCard";
 import {useParams} from "react-router-dom";
 
 function CharacterBiography() {
-    const [characters, setCharacters]=useState('')
-    const {characterId} = useParams()
+    const [characters, setCharacters] = useState('');
+    const {characterId} = useParams();
 
-    useEffect(()=> {
-        async function getData(){
-            try{
-                const result = await axios.get('https://akabab.github.io/superhero-api/api/all.json')
-                    console.log(result.data)
-                    setCharacters(result.data)
-            }catch (e) {
-                console.error(e)
+    useEffect(() => {
+        async function getData() {
+            try {
+                const result = await axios.get('https://akabab.github.io/superhero-api/api/all.json');
+                console.log(result.data);
+                setCharacters(result.data);
+            } catch (e) {
+                console.error(e);
             }
         }
-        getData()
-    }, [])
+
+        getData();
+    }, []);
 
 
-    const currentCharacter = characters.find((character) => {
-        return characterId === character.id
-    })
+    let numberCharacterId = parseInt(characterId, 10);
 
-    console.log(currentCharacter)
+    const currentCharacter = characters && characters.find((character) => {
+        return numberCharacterId === character.id;
+    });
 
-    // const currentCharacter = characters && characters.find((character) => {
-    //     return characterId === character.id
-    // })
-    //
-    // console.log(currentCharacter)
-    //
-    // function checkCharacter() {
-    //     if (characterId === characters.id){
-    //         return characters
-    //     }
-    // }
-    //
-    // console.log(checkCharacter());
+    console.log(currentCharacter);
+
 
     return (
-        <div>
-            <h1>Character biography card component</h1>
-            {/*{characters && characters.map((character) => {*/}
-            {/*    return(*/}
-            {/*        <CharacterBiographyCard*/}
-            {/*            key={character.id}*/}
-            {/*            classname="character-result"*/}
-            {/*            image={character.images.lg}*/}
-            {/*            characterName={character.name}*/}
-            {/*            intelligence={character.powerstats.intelligence}*/}
-            {/*            strength={character.powerstats.strength}*/}
-            {/*            speed={character.powerstats.speed}*/}
-            {/*            durability={character.powerstats.durability}*/}
-            {/*            power={character.powerstats.power}*/}
-            {/*            combat={character.powerstats.combat}*/}
-            {/*            fullName={character.biography.fullName}*/}
-            {/*            gender={character.appearance.gender}*/}
-            {/*            height={character.appearance.height[1]}*/}
-            {/*            hairColor={character.appearance.hairColor}*/}
-            {/*            weight={character.appearance.weight[1]}*/}
-            {/*            eyeColor={character.appearance.eyeColor}*/}
-            {/*            aliases={character.biography.aliases.join(', ')}*/}
-            {/*            placeOfBirth={character.biography.placeOfBirth}*/}
-            {/*            relatives={character.connections.relatives}*/}
-            {/*            firstAppearance={character.biography.firstAppearance}*/}
-            {/*            publisher={character.biography.publisher}*/}
-            {/*        />*/}
-            {/*        )*/}
-            {/*})}*/}
-        </div>
-    );
+        <div className="outer-container-character">
+            <h1>Character biography</h1>
+            {currentCharacter &&
+            <CharacterBiographyCard
+                key={currentCharacter.id}
+                classname="search-result"
+                image={currentCharacter.images.lg}
+                characterName={currentCharacter.name}
+                intelligence={currentCharacter.powerstats.intelligence}
+                strength={currentCharacter.powerstats.strength}
+                speed={currentCharacter.powerstats.speed}
+                durability={currentCharacter.powerstats.durability}
+                power={currentCharacter.powerstats.power}
+                combat={currentCharacter.powerstats.combat}
+                fullName={currentCharacter.biography.fullName}
+                gender={currentCharacter.appearance.gender}
+                height={currentCharacter.appearance.height[1]}
+                hairColor={currentCharacter.appearance.hairColor}
+                weight={currentCharacter.appearance.weight[1]}
+                eyeColor={currentCharacter.appearance.eyeColor}
+                aliases={currentCharacter.biography.aliases.join(', ')}
+                placeOfBirth={currentCharacter.biography.placeOfBirth}
+                relatives={currentCharacter.connections.relatives}
+                firstAppearance={currentCharacter.biography.firstAppearance}
+                publisher={currentCharacter.biography.publisher}
+            />
+            }
+
+        </div>)
 }
 
 export default CharacterBiography;
