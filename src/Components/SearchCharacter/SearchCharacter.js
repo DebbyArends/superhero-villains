c69115249a8c78d1aacd5a4fb39b6b53f3f67059
DebbyArends <debby_arends@live.nl>
2022-06-14 11:16:24 +0200

    Search character aangepast plus styling

--- src/Components/SearchCharacter/SearchCharacter.js
+++ src/Components/SearchCharacter/SearchCharacter.js
@@ -6,145 +6,119 @@ import CharacterBiographyCard from "../CharacterBiographyCard/CharacterBiography
 
 
 function SearchCharacter() {
-    const [superheroData, setSuperheroData] = useState([]);
-    const [oneSuperhero, setOneSuperhero] = useState([])
-    const [id , setId]= useState([])
+    const [allHeroes, setAllHeroes] = useState([]);
+    const [searchMatches, setSearchMatches] = useState([]);
+    const [heroDetails, setHeroDetails] = useState({});
+    const [id, setId] = useState(0)
     const [searchTerm, setSearchTerm] = useState('')
-    // const apiKey = "10228880912034222";
-
-
-
-useEffect(()=> {
-    async function getData(){
-        try{
-            const result = await axios.get(`https://akabab.github.io/superhero-api/api/id/${id}.json`)
-            console.log(result.data)
-            setOneSuperhero(result.data)
-        }catch (e) {
-            console.error(e)
+    // Wanneer de id wordt veranderd willen we de gegevens van die specifieke hero ophalen
+    useEffect(() => {
+        async function getSpecificSuperHero() {
+            try {
+                const result = await axios.get(`https://akabab.github.io/superhero-api/api/id/${id}.json`)
+                console.log(result.data)
+                setHeroDetails(result.data)
+            } catch (e) {
+                console.error(e)
+            }
         }
-    }
-    getData()
-}, [id])
-
-    useEffect(()=> {
-        async function getData(){
-            try{
+        getSpecificSuperHero();
+        // Reset alle oude matches en de ingevoerde zoekopdracht:
+        setSearchMatches([]);
+        setSearchTerm('');
+    }, [id])
+    // Wanneer de pagina laadt, halen we alle helden op zodat we deze kunnen doorzoeken
+    useEffect(() => {
+        async function getData() {
+            try {
                 const result = await axios.get(`https://akabab.github.io/superhero-api/api/all.json`)
                 console.log(result.data)
-                setSuperheroData(result.data)
-            }catch (e) {
+                setAllHeroes(result.data)
+            } catch (e) {
                 console.error(e)
             }
         }
         getData()
-    }, [])
-
-
-    // async function searchSuperHeroes() {
-    //     try {
-    //         const response = await axios.get(`https://www.superheroapi.com/api.php/${apiKey}/search/${searchText}`);
-    //         console.log(response.data.results);
-    //         setSuperheroData(response.data.results);
-    //     } catch (e) {
-    //         console.error(e);
-    //     }
-    // }
-
-    // if (searchTerm.length > 2) {
-    //     searchSuperHeroes();
-    // }
+    }, []);
+    // Als we op enter drukken, willen de de ID setten van de eerste match (ongeacht hoeveel matches er over zijn)
+    function searchHero(e) {
+        e.preventDefault();
+        setId(searchMatches[0].id);
+    }
+    // Wanneer de input veranderd, wordt deze functie uitgevoerd
     function handleChange(e) {
-        setSearchTerm( e.target.value);
-        if (searchTerm.length === 0) {
-            setOneSuperhero([]);
-        }
-        if (searchTerm.length > 2) {
-            let arrayNamesIds = superheroData && superheroData.map((character) =>{
-                return(
-                    {
-                        name: character.name,
-                        id: character.id
-                    }
-                )
-            })
-            console.log(arrayNamesIds);
-
-            const test= arrayNamesIds.find(character=> {
-                return character.name === e.target.value;
-            })
-            setId(test.id);
+        // We slaan de zoekopdracht op in kleine letters
+        const query = e.target.value.toLowerCase();
+        // We zetten de zoekterm in de state;
+        setSearchTerm(query);
+        if (query.length > 2) {
+            // Filter op alle helden
+            const matches = allHeroes.filter((character) => {
+                // En geef een resultaat terug wanneer de naam, in KLEINE letters, een stukje van de zoekterm bevat
+                return character.name.toLowerCase().includes(query);
+            });
+            console.log(matches);
+            // Zet de resultaten (dit kunnen er 0 of 1 of bijvoorbeeld 5 zijn) in de state zodat we deze kunnen laten zien
+            setSearchMatches(matches);
         }
     }
-
-
     return (
         <>
             <div className="main">
-                <input
-                    id="searchBar"
-                    type="search"
-                    placeholder="Search your superhero or villain..."
-                    autoComplete="off"
-                    name="searchBar"
-                    onChange={handleChange}
-                    value={searchTerm}
-                    minLength="2"
-                />
-                {searchTerm.length >0 && searchTerm.length<3 && <p className="error">Search text must be longer than 2 characters</p>}
+                {/*Ik heb er een formulier omheen gezet, zodat de gebruiker ook kan zoeken met Enter!*/}
+                <form onSubmit={searchHero}>
+                    <input
+                        id="searchBar"
+                        type="search"
+                        placeholder="Search your superhero or villain..."
+                        autoComplete="off"
+                        name="searchBar"
+                        onChange={handleChange}
+                        value={searchTerm}
+                        minLength="2"
+                    />
+                    {/*Dit is de auto-complete, hierin mappen we over alle huidige matches heen*/}
+                    <ul className="auto-complete">
+                        {searchMatches.map((searchMatch) => {
+                            return (
+                                // De gebruiker kan erop klikken, dan wordt de id van die hero in de state geplaatst en de gegevens daarover opgehaald
+                                <li key={searchMatch.id} onClick={() => setId(searchMatch.id)}>
+                                    {searchMatch.name}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    {searchTerm.length > 0 && searchTerm.length < 3 &&
+                    <p className="error">Search text must be longer than 2 characters</p>}
+                </form>
             </div>
             <div className="outer-container-character">
-                {/*{superheroData.length > 1? superheroData.map((character) =>*/}
-                {/*    <CharacterBiographyCard*/}
-                {/*        key= {character.id}*/}
-                {/*        classname="search-result"*/}
-                {/*        image={character.images.lg}*/}
-                {/*        characterName={character.name}*/}
-                {/*        intelligence={character.powerstats.intelligence}*/}
-                {/*        strength={character.powerstats.strength}*/}
-                {/*        speed={character.powerstats.speed}*/}
-                {/*        durability={character.powerstats.durability}*/}
-                {/*        power={character.powerstats.power}*/}
-                {/*        combat={character.powerstats.combat}*/}
-                {/*        fullName={character.biography.fullName}*/}
-                {/*        gender={character.appearance.gender}*/}
-                {/*        height={character.appearance.height[1]}*/}
-                {/*        hairColor={character.appearance.hairColor}*/}
-                {/*        weight={character.appearance.weight[1]}*/}
-                {/*        eyeColor={character.appearance.eyeColor}*/}
-                {/*        aliases={character.biography.aliases.join(', ')}*/}
-                {/*        placeOfBirth={character.biography.placeOfBirth}*/}
-                {/*        relatives={character.connections.relatives}*/}
-                {/*        firstAppearance={character.biography.firstAppearance}*/}
-                {/*        publisher={character.biography.publisher}*/}
-                {/*    />*/}
-                {Object.keys(oneSuperhero).length > 0 &&
+                {Object.keys(heroDetails).length > 0 &&
                 <CharacterBiographyCard
-                    key= {oneSuperhero.id}
+                    key={heroDetails.id}
                     classname="search-result"
-                    image={oneSuperhero.images.lg}
-                    characterName={oneSuperhero.name}
-                    intelligence={oneSuperhero.powerstats.intelligence}
-                    strength={oneSuperhero.powerstats.strength}
-                    speed={oneSuperhero.powerstats.speed}
-                    durability={oneSuperhero.powerstats.durability}
-                    power={oneSuperhero.powerstats.power}
-                    combat={oneSuperhero.powerstats.combat}
-                    fullName={oneSuperhero.biography.fullName}
-                    gender={oneSuperhero.appearance.gender}
-                    height={oneSuperhero.appearance.height[1]}
-                    hairColor={oneSuperhero.appearance.hairColor}
-                    weight={oneSuperhero.appearance.weight[1]}
-                    eyeColor={oneSuperhero.appearance.eyeColor}
-                    aliases={oneSuperhero.biography.aliases.join(', ')}
-                    placeOfBirth={oneSuperhero.biography.placeOfBirth}
-                    relatives={oneSuperhero.connections.relatives}
-                    firstAppearance={oneSuperhero.biography.firstAppearance}
-                    publisher={oneSuperhero.biography.publisher}
+                    image={heroDetails.images.lg}
+                    characterName={heroDetails.name}
+                    intelligence={heroDetails.powerstats.intelligence}
+                    strength={heroDetails.powerstats.strength}
+                    speed={heroDetails.powerstats.speed}
+                    durability={heroDetails.powerstats.durability}
+                    power={heroDetails.powerstats.power}
+                    combat={heroDetails.powerstats.combat}
+                    fullName={heroDetails.biography.fullName}
+                    gender={heroDetails.appearance.gender}
+                    height={heroDetails.appearance.height[1]}
+                    hairColor={heroDetails.appearance.hairColor}
+                    weight={heroDetails.appearance.weight[1]}
+                    eyeColor={heroDetails.appearance.eyeColor}
+                    aliases={heroDetails.biography.aliases.join(', ')}
+                    placeOfBirth={heroDetails.biography.placeOfBirth}
+                    relatives={heroDetails.connections.relatives}
+                    firstAppearance={heroDetails.biography.firstAppearance}
+                    publisher={heroDetails.biography.publisher}
                 />}
             </div>
         </>
     );
 }
-
 export default SearchCharacter;
